c7fa1d63c79c2b658ac052c71d60e3d1ac254cc3
yossaj <yossaj@icloud.com>
2019-05-17 23:48:47 +0100

    added a map and some markers

--- src/views/display_view.js
+++ src/views/display_view.js
@@ -21,12 +21,24 @@ DisplayView.prototype.render = function(){
     name.textContent = book.volumeInfo.authors[0]
     this.container.appendChild(title)
     this.container.appendChild(name)
-
-
     const bookCover = document.createElement('img');
     bookCover.src = book.volumeInfo.imageLinks.thumbnail;
     this.container.appendChild(bookCover);
+    
+
+    
     }
+
+    var markerJura = L.marker([56.068, -5.77]).addTo(mymap);
+    var markerHampStead = L.marker([51.558, -0.173]).addTo(mymap);
+    var markerParis = L.marker([48.804, 2.29]).addTo(mymap);
+    markerJura.bindPopup("<b>Orwell wrote</b><br>1984 here.")
+    markerHampStead.bindPopup("<b>Orwell wrote</b><br>Keep the Aspidistra Flying in Hampstead.")
+    markerParis.bindPopup("<b>Here Down and Out</b><br>In Paris And London was written")
+
+  
+
+    
     // this.container.appendChild(air)
     // this.container.appendChild(list)
     // list.appendChild(co)
