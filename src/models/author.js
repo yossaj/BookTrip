364e8c061c3c7597f1fa7b0b1e9af4bd68b25fb1
yossaj <yossaj@icloud.com>
2019-06-02 21:15:35 +0100

    have i broken anything

--- src/models/author.js
+++ src/models/author.js
@@ -18,102 +18,17 @@ Author.prototype.bindEvents = function(){
 }
 
 Author.prototype.getData = function (search, name, channel) {
-    console.log(search,name,channel)
     const request = new RequestHelper(`https://www.googleapis.com/books/v1/volumes?q=${search}&key=${key}`)
 
     request.get()
-        .then((data) => {
-            
+        .then((data) => {  
             const correctAuthor = this.filterAuthor(data.items, `${name}`);
             this.books = correctAuthor
             PubSub.publish(`${channel}`, this.books)
         })
 }
 
-// Author.prototype.getDataGuin = function(){
-//     const request = new RequestHelper(`https://www.googleapis.com/books/v1/volumes?q=ursala+le+guin&key=${key}`)
-    
-//     request.get()
-//     .then((data)=>{
-//         const correctAuthor = this.filterAuthor(data.items, 'Ursula K. Le Guin');
-//         this.books = correctAuthor
-//         PubSub.publish('Guin: Data Ready', this.books)
-//     })
-// }
-
-
-// Author.prototype.getDataDost = function(){
-//     const request = new RequestHelper(`https://www.googleapis.com/books/v1/volumes?q=fyodor+dostoyevsky&key=${key}`)
-    
-//     request.get()
-//     .then((data)=>{
-//         const correctAuthor = this.filterAuthor(data.items, 'Fyodor Dostoyevsky');
-//         this.books = correctAuthor
-//         PubSub.publish('Dost: Data Ready', this.books)
-//     })
-// }
-// Author.prototype.getDataLinyutang = function(){
-//     const request = new RequestHelper(`https://www.googleapis.com/books/v1/volumes?q=lin+yutang&key=${key}`)
-    
-//     request.get()
-//     .then((data)=>{
-//         const correctAuthor = this.filterAuthor(data.items, 'Lin Yutang');
-//         this.books = correctAuthor
-//         PubSub.publish('Lin Yutang: Data Ready', this.books)
-//     })
-// }
-
-// Author.prototype.getDataHemmingway = function(){
-//     const request = new RequestHelper(`https://www.googleapis.com/books/v1/volumes?q=ernest+hemingway&key=${key}`)
-    
-//     request.get()
-//     .then((data)=>{
-//         const correctAuthor = this.filterAuthor(data.items, 'Ernest Hemingway');
-//         this.books = correctAuthor
-//         PubSub.publish('Hemmingway: Data Ready', this.books)
-//     })
-// }
-
-// Author.prototype.getDataOrwell = function(){
-//     const request = new RequestHelper(`https://www.googleapis.com/books/v1/volumes?q=george+orwell&key=${key}`)
-    
-//     request.get()
-//     .then((data)=>{
-//         const correctAuthor = this.filterAuthor(data.items, 'George Orwell');
-//         this.books = correctAuthor
-//         PubSub.publish('Orwell: Data Ready', this.books)
-//     })
-// }
-
-// Author.prototype.getDataNobokov = function(){
-//     const request = new RequestHelper(`https://www.googleapis.com/books/v1/volumes?q=vladimir+nobokov&key=${key}`)
-    
-//     request.get()
-//     .then((data)=>{
-//         const correctAuthor = this.filterAuthor(data.items, 'Vladimir Nabokov');
-//         this.books = correctAuthor
-//         PubSub.publish('Nobokov: Data Ready', this.books)
-//     })
-// }
-
-// Author.prototype.getDataPratchett = function(){
-//     const request = new RequestHelper(`https://www.googleapis.com/books/v1/volumes?q=terry+pratchett&key=${key}`)
-    
-//     request.get()
-//     .then((data)=>{
-//         const correctAuthor = this.filterAuthor(data.items, 'Terry Pratchett');
-//         this.books = correctAuthor
-//         PubSub.publish('Pratchett: Data Ready', this.books)
-//     })
-// }
-
-
-
-
-
 Author.prototype.filterAuthor = function(allBooks, authorName){
-    
-   
     const hasAuthor = this.filterUndefined(allBooks);
     const matchingAuthor = this.filterMatchingAuthor(hasAuthor, authorName)   
     const onlyWriter = matchingAuthor.filter(book => {
